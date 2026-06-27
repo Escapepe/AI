8e797a22354e05538e682d8d51765435476f46bf
Escapepe <1424469396@qq.com>
2026-06-27 13:55:34 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 import base64
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
